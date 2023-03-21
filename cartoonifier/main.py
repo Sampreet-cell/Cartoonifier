9e739ada2c5372f2f92ccf5a403c16bbb0fafa2c
pooranjoyb <pooranjoyb2016@gmail.com>
2023-03-22 00:32:33 +0530

    Added GUI

--- cartoonifier/main.py
+++ cartoonifier/main.py
@@ -6,12 +6,12 @@ import PySimpleGUI as sg
 
 # Image Processing using CV
 
-
+# Method to Read Image from the File Location
 def read_img(filename):
     img = cv2.imread(filename)
     return img
 
-
+# Edge Detections
 def edge_detection(img, line_wdt, blur):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     grayBlur = cv2.medianBlur(gray, blur)
@@ -19,7 +19,7 @@ def edge_detection(img, line_wdt, blur):
         grayBlur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_wdt, blur)
     return edges
 
-
+# Running Color Quantisation
 def color_quantisation(img, k):
     data = np.float32(img).reshape((-1, 3))
     criteria = (cv2.TermCriteria_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
@@ -30,11 +30,6 @@ def color_quantisation(img, k):
     result = result.reshape(img.shape)
     return result
 
-# edgeImg = edge_detection(img, line_wdt, blur_value)
-# img = color_quantisation(img, totalColors)
-# blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
-# cartoon = cv2.bitwise_and(blurred, blurred, mask=edgeImg)
-# cv2.imwrite('output.jpeg', cartoon)
 
 # Graphical User Interface (GUI)
 
@@ -42,45 +37,73 @@ def color_quantisation(img, k):
 # Theme Color
 sg.theme('DarkBlack')
 
-layout = [[sg.Text('W E L C O M  E - T O - C A R T O O N I F I E R')],
-          [sg.Text('Choose the line width : ')],
+layout = [[sg.Text('W E L C O M  E - T O - C A R T O O N I F I E R', font=('Arial Bold', 16))],
+
+          [sg.Text('_'*60)],
+          [sg.Text('Choose the line width : ', font=('Arial Bold', 12))],
           [sg.Slider(range=(1, 50),
                      default_value=25,
                      size=(20, 15),
                      orientation='horizontal',
                      font=('Helvetica', 12))],
-          [sg.Text('Choose the Blur Value : ')],
+          [sg.Text('Choose the Blur Value : ', font=('Arial Bold', 12))],
           [sg.Slider(range=(1, 15),
                      default_value=7,
                      size=(20, 15),
                      orientation='horizontal',
                      font=('Helvetica', 12))],
-          [sg.Text('Choose the Total Colors : ')],
+          [sg.Text('Choose the Total Colors : ', font=('Arial Bold', 12))],
           [sg.Slider(range=(1, 10),
                      default_value=5,
                      size=(20, 15),
                      orientation='horizontal',
                      font=('Helvetica', 12))],
-          [sg.Button('Ok'), sg.Button('Cancel')]]
+          [sg.Text('_'*60)],
+          [sg.Button('Ok', font=('Arial Bold', 12)), sg.Button('Cancel', font=('Arial Bold', 12))]]
+
+layout2 = [[sg.Text(text='Python GUIs for Humans',
+   font=('Arial Bold', 16),
+   size=20, expand_x=True,
+   justification='center')],
+   [sg.Image('./messi.jpeg',
+   expand_x=True, expand_y=True )]
+]
 
 # Create the Window
-window = sg.Window('Cartoonifier', layout)
-
+window = sg.Window('Cartoonifier', layout, element_justification='c')
 
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
 
-    if event == sg.WIN_CLOSED or event == 'Cancel':  
+    if event == sg.WIN_CLOSED or event == 'Cancel':
         # if user closes window or clicks cancel
         break
 
-    filename = sg.popup_get_file('Please Enter the image location  ')
     line_wdt = int(values[0])
     blur_value = int(values[1])
     totalColors = int(values[2])
 
-    print(line_wdt, blur_value, totalColors)
+    # Get image file location
+    filename = sg.popup_get_file('Source File ')
+
+    # Reading image File
+    img = read_img(filename)
+
+    # Edge Detections
+    edgeImg = edge_detection(img, line_wdt, blur_value)
+
+    # Adding Color Quantisation
+    img = color_quantisation(img, totalColors)
+    blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
+
+    # Generating Cartoon
+    cartoon = cv2.bitwise_and(blurred, blurred, mask=edgeImg)
+
+    # Creating Output
+    if event == 'Ok':
+        cv2.imwrite('cartoon.jpeg', cartoon)
+        window.close()
 
 window.close()
